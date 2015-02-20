1f004faa2dcb4e259b4b32a2ffa1a3dbb4bd951f
DerikStenerson <derik.stenerson@outlook.com>
2015-02-19 23:04:14 -0800

    Tutorial: Basics
    1st tutorial

---
+++ ToDoList/ToDoList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ToDoList
+//
+//  Created by Derik Stenerson on 2/19/15.
+//  Copyright (c) 2015 Derik Stenerson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ToDoList/ToDoList/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ToDoList
+//
+//  Created by Derik Stenerson on 2/19/15.
+//  Copyright (c) 2015 Derik Stenerson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
